1e9afa7a02e64ab2ae57b613b91329566dab69f5
Abdullah Kara <abdullahkkaarra@gmail.com>
2025-07-20 14:10:18 +0300

    exception handling starter

---
+++ exception-handling/exception-handling/src/main/java/com/example/exception_handling/entity/Department.java
@@ -0,0 +1,22 @@
+package com.example.exception_handling.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "department")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "location", nullable = false)
+    private String location;
+}
---
+++ exception-handling/exception-handling/src/main/java/com/example/exception_handling/entity/Employee.java
@@ -0,0 +1,23 @@
+package com.example.exception_handling.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Department department;
+}
