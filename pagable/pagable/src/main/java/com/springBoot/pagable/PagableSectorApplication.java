cb66033d08ba5095658a5395cd933db0c216322a
Abdullah Kara <abdullahkkaarra@gmail.com>
2025-07-23 21:24:27 +0300

    verileri set set çekmek için page yapısı oluşturuldu

---
+++ pagable/pagable/src/main/java/com/springBoot/pagable/PagableSectorApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.pagable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagableSectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagableSectorApplication.class, args);
+	}
+
+}
